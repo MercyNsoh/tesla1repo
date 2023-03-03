7733988529dae1b41ea82eeeb6e13c08e0c2d124
MercyNsoh <mercybih678@gmail.com>
2023-03-03 01:58:43 +0000

    conflict

--- usermght.sh
+++ usermght.sh
@@ -1,10 +1,15 @@
 our first apllication
-java apllication is ready 
+java apllication is ready
+<<<<<<< HEAD
 user infos
+<<<<<<< HEAD
 Jesus is lord
 app.java file
 pyn file
 java apllication is ready
 user infos
+=======
+=======
+>>>>>>> parent of 79873f5 (final mergx conflict)
 app.java file
-
+>>>>>>> e4e24995f77536a782d43199583ab7a356f2fc86
