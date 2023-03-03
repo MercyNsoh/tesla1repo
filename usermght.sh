d1df1353651b071412617293105260abe5c48699
MercyNsoh <mercybih678@gmail.com>
2023-03-03 02:00:25 +0000

    Revert "conflict" the conflict has been reolved in another commit This reverts commit 7733988529dae1b41ea82eeeb6e13c08e0c2d124.

--- usermght.sh
+++ usermght.sh
@@ -1,15 +1,10 @@
 our first apllication
-java apllication is ready
-<<<<<<< HEAD
+java apllication is ready 
 user infos
-<<<<<<< HEAD
 Jesus is lord
 app.java file
 pyn file
 java apllication is ready
 user infos
-=======
-=======
->>>>>>> parent of 79873f5 (final mergx conflict)
 app.java file
->>>>>>> e4e24995f77536a782d43199583ab7a356f2fc86
+
